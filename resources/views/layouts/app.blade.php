5a90e9dacfeb894bc790d43e79fc3b88ec969507
hector <hectormotazorrilla@gmail.com>
2023-12-21 11:33:57 -0400

    helper Asset

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,22 +4,22 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="apple-touch-icon" sizes="180x180" href="/../img\icons\logoEntumescenteA.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/../img\icons\logoEntumescenteA.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/../img\icons\logoEntumescenteA.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('img\icons\logoEntumescenteA.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img\icons\logoEntumescenteA.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img\icons\logoEntumescenteA.png') }}">
     <title>
         Especificador de Pintura Intumescente
     </title>
     <!--     Fonts and icons     -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
     <!-- Nucleo Icons -->
-    <link href="/../assets/css/nucleo-icons.css" rel="stylesheet" />
-    <link href="/../assets/css/nucleo-svg.css" rel="stylesheet" />
+    <link href="{{ asset('assets/css/nucleo-icons.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
     <!-- Font Awesome Icons -->
     <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-    <link href="/../assets/css/nucleo-svg.css" rel="stylesheet" />
+    <link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
     <!-- CSS Files -->
-    <link id="pagestyle" href="/../assets/css/argon-dashboard.css" rel="stylesheet" />
+    <link id="pagestyle" href="{{ asset('assets/css/argon-dashboard.css') }}" rel="stylesheet" />
     <style>
         .page-content {
             min-height: calc(100vh - 10px);
@@ -38,7 +38,7 @@
     @endguest
     @auth
         <div class="position-absolute w-100 min-height-300 top-0"
-            style="background-image: url('/../assets/img/profile-layout-header.jpg'); background-position-y: 50%;">
+            style="background-image: url("{{ asset('assets/img/profile-layout-header.jpg') }}"); background-position-y: 50%;">
             <span class="mask bg-primary opacity-3"></span>
         </div>
         @include('layouts.navbars.auth.sidenav')
@@ -48,10 +48,10 @@
         {{-- @include('components.fixed-plugin') --}}
     @endauth
     <!--   Core JS Files   -->
-    <script src="/../assets/js/core/popper.min.js"></script>
-    <script src="/../assets/js/core/bootstrap.min.js"></script>
-    <script src="/../assets/js/plugins/perfect-scrollbar.min.js"></script>
-    <script src="/../assets/js/plugins/smooth-scrollbar.min.js"></script>
+    <script src="{{ asset('assets/js/core/popper.min.js') }}"></script>
+    <script src="{{ asset('assets/js/core/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/perfect-scrollbar.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/smooth-scrollbar.min.js') }}"></script>
     <script>
         var win = navigator.platform.indexOf('Win') > -1;
         if (win && document.querySelector('#sidenav-scrollbar')) {
@@ -61,7 +61,7 @@
             Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
         }
     </script>
-    <script src="/../assets/js/argon-dashboard.js"></script>
+    <script src="{{ asset('assets/js/argon-dashboard.js') }}"></script>
     @auth
         @yield('js')
     @endauth
